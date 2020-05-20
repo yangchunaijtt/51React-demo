6117ffb7630f6b292ada94d647138b2827888ef6
yangchunaijtt <1831127026@qq.com>
2020-05-20 16:00:37 +0800

    5.20

--- todo-list2-redux/src/components/Foot.jsx
+++ todo-list2-redux/src/components/Foot.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import store from "../store/index"
-import { deledAllFinshed,footCheckAll } from "../store/actions"
+import { deledAllFinshed, footCheckAll } from "../store/actions"
 
 
 export default class Foot extends Component {
@@ -11,15 +11,15 @@ export default class Foot extends Component {
         store.subscribe(this.storeChange) // 订阅Redux的状态
         this.inputRef = React.createRef();
     };
-    _selected(){
+    _selected() {
         let checked = this.state.isChecked;
-        this.setState({
-            isChecked:!checked
-        })
         const action = footCheckAll(!checked);
         store.dispatch(action);
+        this.setState({
+            isChecked: !checked
+        })
     };
-    _deleAllFinsed(){
+    _deleAllFinsed() {
         const action = deledAllFinshed();
         store.dispatch(action);
     };
@@ -27,21 +27,21 @@ export default class Foot extends Component {
         this.setState(store.getState())
     };
     render() {
-        const {todos, finishedCount,isChecked} = this.state;
+        const { todos, finishedCount, isChecked } = this.state;
         return (
             <div className="todo-footer">
                 <label>
-                    <input type="checkbox" 
-                    checked={isChecked}
-                    onChange={() => { this._selected()}}
+                    <input type="checkbox"
+                        checked={todos.length ? isChecked : false}
+                        onChange={() => { this._selected() }}
                     />
                 </label>
                 <span>
-                 <span>已完成{finishedCount}件</span> / 总计{todos.length}件
+                    <span>已完成{finishedCount}件</span> / 总计{todos.length}件
                 </span>
-                <button 
-                onClick={() =>this._deleAllFinsed()}
-                className="btn btn-warning">清除已完成任务</button>
+                <button
+                    onClick={() => this._deleAllFinsed()}
+                    className="btn btn-warning">清除已完成任务</button>
             </div>
         )
     }
