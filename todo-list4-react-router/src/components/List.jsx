d7590565d58b2182def2b761115246158b5b3f12
yangchunaijtt <1831127026@qq.com>
2020-06-01 16:49:22 +0800

    6.1

---
+++ todo-list4-react-router/src/components/List.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import Item from "./Item"
+import { connect } from 'react-redux'
+
+class List extends Component {
+    constructor(props) {
+        super(props);
+    };
+    render() {
+        const {todos }= this.props;
+        return (
+            <ul className="todo-main">
+                {todos.map((item, index) => (
+                    <Item
+                        key={item.id}
+                        todItem={item} />
+                ))}
+            </ul>
+        )
+
+    };
+}
+//  将state映射到Counter组件的props
+const mapStateToProps = (state) => {
+    return {
+        todos: state.todos
+    }
+}
+
+
+
+export default connect(
+    mapStateToProps,
+    null
+)(List)
