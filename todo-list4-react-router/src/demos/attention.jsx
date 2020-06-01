cf07ba17f42f9d5c50aa0b7aceb259336b85323c
yangchunaijtt <1831127026@qq.com>
2020-06-01 17:41:22 +0800

    6.1

---
+++ todo-list4-react-router/src/demos/attention.jsx
@@ -0,0 +1,13 @@
+import React, { Components } from "react";
+
+class attention extends Components {
+    render() {
+        return (
+            <div>
+                关注页
+            </div>
+        )
+    }
+}
+
+export default attention;
